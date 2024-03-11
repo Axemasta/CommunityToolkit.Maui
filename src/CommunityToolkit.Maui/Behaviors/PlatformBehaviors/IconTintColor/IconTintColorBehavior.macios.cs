ef974c3f269beb463b60c420233cb846680feb1a
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-03-11 10:40:42 +0000

    Work in progress ios brush gradient
    Having a test to see what is possible on ios to render brushed ontop of UIImages

--- src/CommunityToolkit.Maui/Behaviors/PlatformBehaviors/IconTintColor/IconTintColorBehavior.macios.cs
+++ src/CommunityToolkit.Maui/Behaviors/PlatformBehaviors/IconTintColor/IconTintColorBehavior.macios.cs
@@ -1,4 +1,8 @@
 ﻿using System.ComponentModel;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using Microsoft.Maui.Controls.Platform;
 using Microsoft.Maui.Platform;
 using UIKit;
 
@@ -9,7 +13,15 @@ public partial class IconTintColorBehavior
 	/// <inheritdoc/>
 	protected override void OnAttachedTo(View bindable, UIView platformView)
 	{
-		ApplyTintColor(platformView, bindable, TintColor);
+		if (TintColor is not null)
+		{
+			ApplyTintColor(platformView, bindable, TintColor);
+		}
+		
+		if (TintGradient is not null)
+		{
+			ApplyTintGradient(platformView, bindable, TintGradient);
+		}
 
 		bindable.PropertyChanged += OnElementPropertyChanged;
 		this.PropertyChanged += (s, e) =>
@@ -17,6 +29,11 @@ public partial class IconTintColorBehavior
 			if (e.PropertyName == TintColorProperty.PropertyName)
 			{
 				ApplyTintColor(platformView, bindable, TintColor);
+			}
+
+			if (e.PropertyName == TintGradientProperty.PropertyName)
+			{
+				ApplyTintGradient(platformView, bindable, TintGradient);
 			}
 		};
 	}
@@ -119,4 +136,236 @@ public partial class IconTintColorBehavior
 		imageView.TintColor = color.ToPlatform();
 	}
 
+	void ApplyTintGradient(UIView platformView, View element, Brush? brush)
+	{
+		if (brush is null)
+		{
+			ClearTintColor(platformView, element);
+			return;
+		}
+		
+		switch (platformView)
+		{
+			case UIImageView imageView:
+				SetUIImageViewTintColor(imageView, element, brush);
+				break;
+			case UIButton button:
+				// SetUIButtonTintColor(button, element, brush);
+				break;
+			default:
+				throw new NotSupportedException($"{nameof(IconTintColorBehavior)} only currently supports {nameof(UIButton)} and {nameof(UIImageView)}.");
+		}
+	}
+	
+	static void SetUIImageViewTintColor(UIImageView imageView, View element, Brush brush)
+	{
+		if (imageView.Image is null)
+		{
+			return;
+		}
+
+		var renderedImage = imageView.Image.RenderGradient(brush);
+
+		if (renderedImage is not null)
+		{
+			imageView.Image = renderedImage;
+		}
+	}
+}
+
+static class UIImageExtension
+{
+	const string backgroundLayer = "BackgroundLayer";
+	
+	internal static UIImage? RenderGradient(this UIImage image, UIColor[] colors)
+	{
+		var source = image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate).CGImage;
+
+		if (source is null)
+		{
+			return null;
+		}
+
+		var size = image.Size;
+
+		var cgColors = new CGColor[colors.Length];
+
+		for (int i = 0; i < colors.Length; i++)
+		{
+			cgColors[i] = colors[i].CGColor; 
+		}
+		
+		var space = CGColorSpace.CreateDeviceRGB();
+
+		var gradient = new CGGradient(space, cgColors);
+
+		using var renderer = new UIGraphicsImageRenderer(size);
+		
+		var resultImage = renderer.CreateImage((context =>
+		{
+			var cgContext = context.CGContext;
+			cgContext.TranslateCTM(0, size.Height);
+			cgContext.ScaleCTM(1, -1);
+			
+			cgContext.SetBlendMode(CGBlendMode.Normal);
+
+			var rect = new CGRect(0, 0, size.Width, size.Height);
+			
+			cgContext.ClipToMask(rect, source);
+			cgContext.DrawLinearGradient(gradient, CGPoint.Empty, new CGPoint(0, size.Height), CGGradientDrawingOptions.DrawsAfterEndLocation);
+		}));
+
+		return resultImage;
+	}
+	
+	internal static UIImage? RenderGradient(this UIImage image, Brush brush)
+	{
+		var source = image.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate).CGImage;
+
+		if (source is null)
+		{
+			return null;
+		}
+
+		var size = image.Size;
+
+		var gradient = GetBrushGradient(image, brush);
+
+		using var renderer = new UIGraphicsImageRenderer(size);
+		
+		var resultImage = renderer.CreateImage((context =>
+		{
+			var cgContext = context.CGContext;
+			cgContext.TranslateCTM(0, size.Height);
+			cgContext.ScaleCTM(1, -1);
+			
+			cgContext.SetBlendMode(CGBlendMode.Normal);
+
+			var rect = new CGRect(0, 0, size.Width, size.Height);
+			
+			cgContext.ClipToMask(rect, source);
+			cgContext.DrawLinearGradient(gradient, CGPoint.Empty, new CGPoint(0, size.Height), CGGradientDrawingOptions.DrawsAfterEndLocation);
+		}));
+
+		return resultImage;
+	}
+
+	static CGGradient? GetBrushGradient(UIImage? image, Brush brush)
+	{
+		if (image is null)
+		{
+			return null;
+		}
+
+		if (brush is SolidColorBrush solidColorBrush)
+		{
+			
+		}
+
+		if (brush is LinearGradientBrush linearGradientBrush)
+		{
+			var p1 = linearGradientBrush.StartPoint;
+			var p2 = linearGradientBrush.EndPoint;
+			
+			
+
+			if (linearGradientBrush.GradientStops is null || linearGradientBrush.GradientStops.Count == 0)
+			{
+				return null;
+			}
+			
+			var orderedStops = linearGradientBrush.GradientStops.OrderBy(x => x.Offset).ToList();
+			
+			var space = CGColorSpace.CreateDeviceRGB();
+			var colors = GetCAGradientLayerColors(orderedStops);
+			// var locations = GetCAGradientLayerLocations(orderedStops);
+
+			var gradient = new CGGradient(space, colors);
+
+			// if (linearGradientBrush.GradientStops != null && linearGradientBrush.GradientStops.Count > 0)
+			// {
+			// 	var orderedStops = linearGradientBrush.GradientStops.OrderBy(x => x.Offset).ToList();
+			// 	linearGradientLayer.Colors = GetCAGradientLayerColors(orderedStops);
+			// 	linearGradientLayer.Locations = GetCAGradientLayerLocations(orderedStops);
+			// }
+
+			return gradient;
+		}
+
+		if (brush is RadialGradientBrush radialGradientBrush)
+		{
+			
+		}
+
+		return null;
+	}
+	
+	static CGColor[] GetCAGradientLayerColors(List<GradientStop> gradientStops)
+	{
+		if (gradientStops == null || gradientStops.Count == 0)
+		{
+			return new CGColor[0];
+		}
+
+		CGColor[] colors = new CGColor[gradientStops.Count];
+
+		int index = 0;
+		foreach (var gradientStop in gradientStops)
+		{
+			if (gradientStop.Color == Colors.Transparent)
+			{
+				var color = gradientStops[index == 0 ? index + 1 : index - 1].Color;
+				CGColor nativeColor = color.ToPlatform().ColorWithAlpha(0.0f).CGColor;
+				colors[index] = nativeColor;
+			}
+			else
+			{
+				colors[index] = gradientStop.Color.ToCGColor();
+			}
+
+			index++;
+		}
+
+		return colors;
+	}
+	
+	static NSNumber[] GetCAGradientLayerLocations(List<GradientStop> gradientStops)
+	{
+		if (gradientStops == null || gradientStops.Count == 0)
+		{
+			return new NSNumber[0];
+		}
+
+		if (gradientStops.Count > 1 && gradientStops.Any(gt => gt.Offset != 0))
+		{
+			return gradientStops.Select(x => new NSNumber(x.Offset)).ToArray();
+		}
+		else
+		{
+			int itemCount = gradientStops.Count;
+			int index = 0;
+			float step = 1.0f / itemCount;
+
+			NSNumber[] locations = new NSNumber[itemCount];
+
+			foreach (var gradientStop in gradientStops)
+			{
+				float location = step * index;
+				bool setLocation = !gradientStops.Any(gt => gt.Offset > location);
+
+				if (gradientStop.Offset == 0 && setLocation)
+				{
+					locations[index] = new NSNumber(location);
+				}
+				else
+				{
+					locations[index] = new NSNumber(gradientStop.Offset);
+				}
+
+				index++;
+			}
+
+			return locations;
+		}
+	}
 }
--- src/CommunityToolkit.Maui/Behaviors/PlatformBehaviors/IconTintColor/IconTintColorBehavior.shared.cs
+++ src/CommunityToolkit.Maui/Behaviors/PlatformBehaviors/IconTintColor/IconTintColorBehavior.shared.cs
@@ -10,6 +10,12 @@ public partial class IconTintColorBehavior : PlatformBehavior<View>
 	/// </summary>
 	public static readonly BindableProperty TintColorProperty =
 		BindableProperty.Create(nameof(TintColor), typeof(Color), typeof(IconTintColorBehavior), default);
+	
+	/// <summary>
+	/// Attached Bindable Property for the <see cref="TintGradient"/>.
+	/// </summary>
+	public static readonly BindableProperty TintGradientProperty =
+		BindableProperty.Create(nameof(TintGradient), typeof(Brush), typeof(IconTintColorBehavior), default);
 
 	/// <summary>
 	/// Property that represents the <see cref="Color"/> that Icon will be tinted.
@@ -18,5 +24,14 @@ public partial class IconTintColorBehavior : PlatformBehavior<View>
 	{
 		get => (Color?)GetValue(TintColorProperty);
 		set => SetValue(TintColorProperty, value);
+	}
+
+	/// <summary>
+	/// Property that represents the <see cref="Brush"/> that Icon will be tinted.
+	/// </summary>
+	public Brush? TintGradient
+	{
+		get => (Brush?)GetValue(TintGradientProperty);
+		set => SetValue(TintGradientProperty, value);
 	}
 }
